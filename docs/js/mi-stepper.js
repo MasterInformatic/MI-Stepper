6eeea7ccd4921ca4d64e2365aaa0be5e42e57afe
MasterInformatic <tutoriales.flutter.yt@gmail.com>
2020-02-18 21:11:50 -0600

    added step validation

--- docs/js/mi-stepper.js
+++ docs/js/mi-stepper.js
@@ -45,33 +45,43 @@
 	  showTab.call(this);
   }
 
-  function showTab(index){
-  	
-	if(this.direction === 'next'){
-		this.currentTab = this.currentTab + 1;
-		if(this.currentTab >= this.tabs.length){
-			this.currentTab = (this.tabs.length-1);
-		}
-		this.tabs[this.currentTab-1].style.display = 'none';
-	}else if(this.direction === 'prev'){
-		this.currentTab = this.currentTab - 1;
-		if(this.currentTab <= 0){
-			this.currentTab = 0;
-		}
-		this.tabs[this.currentTab+1].style.display = 'none';
-	}else if(this.direction === 'target'){
-		this.tabs[this.currentTab].style.display = 'none';
-		this.tabs[index].style.display = 'block';
-		this.currentTab = index;
-	}else{
-		this.tabs[this.currentTab].style.display = 'none';
-	}
 
-	this.tabs[this.currentTab].style.display = 'block';
-	setIndicator.call(this);
-	this.options.onChange(event, this.currentTab);
-	butonHandle.call(this);
-	finish.call(this);
+  function showTab(index){
+
+  	var validate = true;
+  	if(this.options.linear){
+	  	if(this.options.onChange(event, this.currentTab) == false){
+	  		validate = false;
+	  	}
+  	}
+
+		if(this.direction === 'next'){
+			if(validate){
+				this.currentTab = this.currentTab + 1;
+				if(this.currentTab >= this.tabs.length){
+					this.currentTab = (this.tabs.length-1);
+				}
+				this.tabs[this.currentTab-1].style.display = 'none';
+			}
+		}else if(this.direction === 'prev'){
+			this.currentTab = this.currentTab - 1;
+			if(this.currentTab <= 0){
+				this.currentTab = 0;
+			}
+			this.tabs[this.currentTab+1].style.display = 'none';
+		}else if(this.direction === 'target'){
+			this.tabs[this.currentTab].style.display = 'none';
+			this.tabs[index].style.display = 'block';
+			this.currentTab = index;
+		}else{
+			this.tabs[this.currentTab].style.display = 'none';
+		}
+
+		this.tabs[this.currentTab].style.display = 'block';
+		setIndicator.call(this);
+		this.options.onChange(event, this.currentTab);
+		butonHandle.call(this);
+		finish.call(this);
   }
 
   function butonHandle(){
@@ -125,7 +135,6 @@
   	}
   }
 
-
   function finish(){
 	if(this.currentTab == (this.tabs.length-1)){
 		this.options.onFinish.call();
@@ -147,6 +156,5 @@
 	}
 	return source;
   }
-
 
 }());
